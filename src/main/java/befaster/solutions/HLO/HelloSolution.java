7ec2bd40149634795d2b3491d4dca41443116d0f
sbx_user1051 <sbx_user1051@169.254.18.25>
2019-05-07 22:54:50 +0000

    Tue May 07 22:54:50 UTC 2019

--- src/main/java/befaster/solutions/HLO/HelloSolution.java
+++ src/main/java/befaster/solutions/HLO/HelloSolution.java
@@ -6,8 +6,9 @@ public class HelloSolution {
 	//This function takes a String[] parameter and returns the friend name passed to the function.
 	public String hello(String friendName) {
         
-    	return "Hello World!";
+    	return "Hello, World!";
     }
 	
 }
+
 
