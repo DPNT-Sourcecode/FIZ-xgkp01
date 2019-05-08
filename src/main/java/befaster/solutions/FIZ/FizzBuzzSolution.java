984bd9380e4ba7c278ede5bac784196c60a94b0c
sbx_user1051 <sbx_user1051@169.254.185.213>
2019-05-08 13:26:33 +0000

    Wed May 08 13:26:33 UTC 2019

--- src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
+++ src/main/java/befaster/solutions/FIZ/FizzBuzzSolution.java
@@ -2,8 +2,74 @@ package befaster.solutions.FIZ;
 
 public class FizzBuzzSolution {
 	
-    public String fizzBuzz(Integer number) {
+	/*
+	  - A number is "fizz" if it is divisible by 3 or if it has a 3 in it
+	  - A number is "buzz" if it is divisible by 5 or if it has a 5 in it
+	  - A number can be both "fizz" and "buzz" at the same time. If this happens then write "fizz buzz"
+	  */
+	
+	public String fizzBuzz(Integer number) {
+		
+		String result = "";
+		
+		String numVal = number.toString();
+		
+		
+		boolean containsThree = containsThreeOrFive(numVal, '3');
+		boolean containsFive = containsThreeOrFive(numVal, '5');
+		
+		
+        if ((number % 3 == 0) || (containsThree)) {
+    		
+    		result = "fizz";
+    	
+        }
         
+        else if ((number % 5 == 0) || (containsFive)) {
+    		
+    		result = "buzz";
+    	
+        }
+        
+        if ((number % 3 == 0 && number % 5 == 0) ||
+        	 (containsThree && containsFive)) {
+    		
+    		result = "fizz buzz";
+    		
+    	}
+		
+		
+		return result;
+		
+		
+	}
+	
+	//Method to check if the string value contains the number 3 or 5
+	private boolean containsThreeOrFive(String numVal, char chDigit) {
+		
+		boolean containsVal = false;
+		
+		for (int i = 0; i < numVal.length(); i++) {
+			
+			char ch = numVal.charAt(i);
+			
+			if (ch == chDigit) {
+				
+				containsVal= true;
+				break;
+			}
+			
+		}
+		
+		return containsVal;
+	}
+	
+	
+	
+	
+	/*
+	 public String fizzBuzz(Integer number) {
+    	
     	String result = "";
     	boolean divThreeorFive = false;
     	
@@ -37,5 +103,7 @@ public class FizzBuzzSolution {
     	return result;
     	
     }
+    */
 
 }
+
